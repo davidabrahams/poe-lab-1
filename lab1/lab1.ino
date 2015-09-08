1fa17648c4ff2ae2c13f43bcb71e830a108deb74
Jayce Chow <gigichow@gigis-air.olin.edu>
2015-09-08 12:23:20 -0400

    final code end of class 9_8_15

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -13,15 +13,21 @@
   by Scott Fitzgerald
  */
 
+const int inPin = 8;
+const int outPins[] = {9, 10, 11, 12, 13};
+
+int prevState = LOW;
+int state = LOW;
+int pressCount = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode(inPin, INPUT);
+  for (int i = 0; i < sizeof(outPins); i++) {
+    pinMode(outPins[i], OUTPUT);
+  }
+  Serial.begin(9600);
 }
 
 void allOn() {
@@ -40,11 +46,13 @@ void bounce() {
   allOff();
   for (int thisPin = 9; thisPin <= 12; thisPin++) {
     digitalWrite(thisPin, HIGH);
+    checkButton();
     delay(50);
     digitalWrite(thisPin, LOW);
   }
   for (int thisPin = 13; thisPin >= 10; thisPin--) {
     digitalWrite(thisPin, HIGH);
+    checkButton();
     delay(50);
     digitalWrite(thisPin, LOW);
   }
@@ -54,20 +62,55 @@ void allBlink() {
   allOff();
   for (int thisPin = 9; thisPin <= 13; thisPin++) {
     digitalWrite(thisPin, HIGH);
-    delay(1000);
+    checkButton();
+    delay(100);
     digitalWrite(thisPin, LOW);
-    delay(1000);
   }
 }
 
 void allFlash() {
   allOn();
-  delay(1000);
+  checkButton();
+  delay(100);
   allOff();
-  delay(1000);
+  checkButton();
+  delay(100);
+}
+
+void checkButton() {
+  state = digitalRead(8);
+
+  if (state != prevState) {
+    if (state == HIGH) {
+      pressCount++;
+    }
+  }
+
+  prevState = state;
+}
+
+void cycle() {
+
+  checkButton();
+  
+  if (pressCount % 5 == 0) {
+    allOn();
+  }
+  if (pressCount % 5 == 1) {
+    allOff();
+  }
+  if (pressCount % 5 == 2) {
+    allBlink();
+  }
+  if (pressCount % 5 == 3) {
+    bounce();
+  }
+  if (pressCount % 5 == 4) {
+    allFlash();
+  }
 }
 
 //// the loop function runs over and over again forever
 void loop() {
-  bounce();
+  cycle();
 }
